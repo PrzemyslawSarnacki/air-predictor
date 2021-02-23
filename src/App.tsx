ef42799026cd20e542dc80925a9cda25ecc0f6e4
PrzemyslawSarnacki <przemos100@gmail.com>
2021-02-23 22:14:21 +0100

    Stripping accents and conditional aqi

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { csv, DSVParsedArray } from 'd3';
 import LineChart from './LineChart';
 
+var S = require('string');
+
 const row = (d: any) => {
   d.y_pred = +d.y_pred;
   d.y_pred_std = +d.y_pred_std;
@@ -21,7 +23,8 @@ const App: React.FC = () => {
   const [labels,
     setLabels] = useState<Array<[string]>>();
   const [lastDate, setLastDate] = useState<Array<[string]>>();
-  const [city, setCity] = useState<string>("katowice");
+  const [city, setCity] = useState<string>("Katowice");
+  const [aqi, setAqi] = useState<number>(0);
 
   const getPredictionData = (city: string) => csv(`https://raw.githubusercontent.com/PrzemyslawSarnacki/AirQualityPrediction/master/data/predictions/history-${city}.csv`, row).then(
     (data) => {
@@ -42,42 +45,40 @@ const App: React.FC = () => {
   );
 
   const fetchCity = (city: string) => {
-    const url = 'https://api.openaq.org/v1/measurements?city=Warszawa';
-const options = {
-  
+    const url = `https://api.openaq.org/v1/measurements?city=${city}&parameter=pm25`;
+    fetch(url)
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
 
-  method: 'get',
-  
-};
-fetch(url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+          response.json().then(function (data) {
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data.results[0].value);
+            setAqi(data.results[0].value);
+          });
+        }
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
       });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
 
 
-  } 
+  }
+
+  const stripAccents = (city: string) => {
+    return S(city).latinise().s.toLowerCase()
+  }
 
 
   useEffect(() => {
     xlabels = [];
     xdata = [];
-    getHistoricData(city);
-    getPredictionData(city);
-    fetchCity("");
+    getHistoricData(stripAccents(city));
+    getPredictionData(stripAccents(city));
+    fetchCity(city);
   }, [city]);
 
 
@@ -91,14 +92,19 @@ fetch(url)
       <div className="App-chart">
         <h6>Choose a city:</h6>
         <select onChange={(option) => setCity(option.target.value)}>
-          <option>katowice</option>
-          <option>bialystok</option>
-          <option>warszawa</option>
-          <option>poznan</option>
-          <option>krakow</option>
+          <option>Katowice</option>
+          <option>Białystok</option>
+          <option>Warszawa</option>
+          <option>Poznań</option>
+          <option>Kraków</option>
         </select>
 
         <LineChart labels={xlabels} xdata={xdata} />
+        <p>{aqi}</p>
+        {aqi > 60 ? 
+        <p>sadFace</p>:
+        <p>happyFace</p>
+      }
       </div>
       <div className="App-empty">
       </div>
