35a78f320ab267044a04a7d928a2811e902a3268
PrzemyslawSarnacki <przemos100@gmail.com>
2021-02-24 21:58:45 +0100

    Merge complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { csv, DSVParsedArray } from 'd3';
 import LineChart from './LineChart';
 
+var S = require('string');
+
 const row = (d: any) => {
   d.y_pred = +d.y_pred;
   d.y_pred_std = +d.y_pred_std;
@@ -17,20 +19,14 @@ var xlabels: Array<[string]> = [];
 
 const App: React.FC = () => {
 
-  const [historyData, setHistoryData] = useState<Array<[number]>>();
-  const [labels,
-    setLabels] = useState<Array<[string]>>();
-  const [lastDate, setLastDate] = useState<Array<[string]>>();
-  const [city, setCity] = useState<string>("katowice");
+  const [city, setCity] = useState<string>("Katowice");
+  const [aqi, setAqi] = useState<number>(0);
 
   const getPredictionData = (city: string) => csv(`https://raw.githubusercontent.com/PrzemyslawSarnacki/AirQualityPrediction/master/data/predictions/history-${city}.csv`, row).then(
     (data) => {
 
       data.forEach((object) => xlabels.push(object[""]));
       data.forEach((object) => xdata.push(object["y_pred"]));
-      setLastDate(data[0][""]);
-      setLabels(xlabels);
-      setHistoryData(xdata);
     }
   );
 
@@ -41,11 +37,56 @@ const App: React.FC = () => {
     }
   );
 
+  const fetchCity = (city: string) => {
+    const url = `https://api.openaq.org/v1/measurements?city=${city}&parameter=pm25`;
+    fetch(url)
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+
+          response.json().then(function (data) {
+
+            setAqi(data.results[0].value);
+          });
+        }
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+
+
+  }
+
+  const stripAccents = (city: string) => {
+    return S(city).latinise().s.toLowerCase()
+  }
+
+  const categorize = () => {
+    if (aqi < 50) {
+      return <p>happyFace</p>
+    }
+    else if (aqi > 50 && aqi < 100) {
+      return <p>sadFace</p>
+    }
+    else if (aqi > 100 && aqi < 200) {
+      return <p>verysadFace</p>
+    }
+    else if (aqi > 200) {
+      return <p>tragicFace</p>
+    }
+  }
+
+
   useEffect(() => {
     xlabels = [];
     xdata = [];
-    getHistoricData(city);
-    getPredictionData(city);
+    getHistoricData(stripAccents(city));
+    getPredictionData(stripAccents(city));
+    fetchCity(city);
   }, [city]);
 
 
@@ -59,14 +100,17 @@ const App: React.FC = () => {
       <div className="App-chart">
         <h6>Choose a city:</h6>
         <select onChange={(option) => setCity(option.target.value)}>
-          <option>katowice</option>
-          <option>bialystok</option>
-          <option>warszawa</option>
-          <option>poznan</option>
-          <option>krakow</option>
+          <option>Katowice</option>
+          <option>Białystok</option>
+          <option>Warszawa</option>
+          <option>Poznań</option>
+          <option>Kraków</option>
         </select>
 
         <LineChart labels={xlabels} xdata={xdata} />
+        <p>Data from within 1 hour:</p>
+        <p>{aqi}</p>
+        {categorize()}
       </div>
       <div className="App-empty">
       </div>
