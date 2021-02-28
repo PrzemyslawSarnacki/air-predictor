59498976205e851222b9d9a637842e0ba7ec1903
PrzemyslawSarnacki <przemos100@gmail.com>
2021-02-28 21:50:19 +0100

    Switched UI components to Rainblur style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect, ReactNode } from 'react';
 import './css/tailwind.css';
 import { csv } from 'd3';
 import LineChart from './LineChart';
@@ -68,58 +67,123 @@ const App: React.FC = () => {
 
   const categorize = () => {
     if (aqi < 50) {
-      return <p>🥳</p>
+      return <p className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">🥳</p>
     }
     else if (aqi > 50 && aqi < 100) {
-      return <p>😒</p>
+      return <p className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">😒</p>
     }
-    else if (aqi > 100 && aqi < 200){
-      return <p>😥</p>
+    else if (aqi > 100 && aqi < 200) {
+      return <p className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">😥</p>
     }
-    else if (aqi > 200){
-      return <p>😰</p>
+    else if (aqi > 200) {
+      return <p className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">😰</p>
     }
 
 
-}
+  }
 
 
-useEffect(() => {  
-  xlabels = [];
-  xdata = [];
-  getHistoricData(stripAccents(city));
-  getPredictionData(stripAccents(city));
-  fetchCity(city);
-}, [city]);
+  useEffect(() => {
+    xlabels = [];
+    xdata = [];
+    getHistoricData(stripAccents(city));
+    getPredictionData(stripAccents(city));
+    fetchCity(city);
+  }, [city]);
 
 
-return (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Choose your city
-        </p>
-      </header>
-      <div className="App-chart">
-        <h6>Choose a city:</h6>
-        <select onChange={(option) => setCity(option.target.value)}>
-          <option>Katowice</option>
-          <option>Białystok</option>
-          <option>Warszawa</option>
-          <option>Poznań</option>
-          <option>Kraków</option>
-        </select>
+  return (
+    <body className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed" style={{ backgroundImage: `url(https://raw.githubusercontent.com/tailwindtoolbox/Rainblur-Landing-Page/main/header.png)` }}>
+      <div className="h-full">
+        <div className="w-full container mx-auto">
+          <div className="w-full flex items-center justify-between">
+            <a className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+              Air<span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">Predictor</span>
+            </a>
 
-        <LineChart labels={xlabels} xdata={xdata} />
-        <p>Data from within 1 hour:</p>
-        <p>{aqi}</p>
-        {categorize()}
+            <div className="flex w-1/2 justify-end content-center">
+              <a className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out" href="https://twitter.com/intent/tweet?url=#">
+                <svg className="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <path
+                    d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"
+                  ></path>
+                </svg>
+              </a>
+              <a
+                className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out"
+                href="https://www.facebook.com/sharer/sharer.php?u=#"
+              >
+                <svg className="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+            <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+              Check
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                Your City
+            </span>
+            to protect yourself
+          </h1>
+            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+              Check predicted air quality index for your city so you can keep your healt on point.
+          </p>
+          </div>
+
+          <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-blue-300 py-2 font-bold mb-2">
+                Choose a city:
+              </label>
+              <select
+                className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                onChange={(option) => setCity(option.target.value)}>
+                <option>Katowice</option>
+                <option>Białystok</option>
+                <option>Warszawa</option>
+                <option>Poznań</option>
+                <option>Kraków</option>
+              </select>
+
+            </div>
+
+            <div className="flex items-center justify-between pt-4">
+              <button
+                className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
+            <div>
+
+              <LineChart labels={xlabels} xdata={xdata} />
+            </div>
+          </form>
+          <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
+            <label className="block text-blue-300 py-2 font-bold mb-2">
+              Data from within 1 hour:
+              </label>
+            <p className="leading-normal text-blue-300 md:text-2xl mb-8 text-center md:text-left">
+
+              {aqi}</p>
+            {categorize()}
+          </form>
+        </div>
       </div>
-      <div className="App-empty">
-      </div>
-    </div>
 
-);
+      <div className="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+        <a className="text-gray-500 no-underline hover:no-underline" href="#">&copy; App 2020</a>
+          - Template by
+          <a className="text-gray-500 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com">TailwindToolbox.com</a>
+      </div>
+    </body>
+  );
 }
 
 export default App;
